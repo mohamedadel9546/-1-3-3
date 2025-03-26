9e8dccf20954dcc5a42ca814ac2abbda0d167c7d
mohamedadel9546 <adampmvdj@gmail.com>
2025-03-26 02:35:12 +0200

    لو عايز تعرف الوقت الحلقه الاخيرة من الكورس 2

---
+++ معرفه الوقت.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <ctime>
+#include <fstream>
+#include <vector>
+#include <string>
+#pragma warning (disable:4996)
+using namespace std;
+
+
+
+int main() { 
+  
+  time_t	t = time(0);
+  char* dt = ctime(&t);
+  cout << "local time" << dt << endl;
+
+  tm* tt = gmtime(&t);
+  dt = asctime(tt);
+  cout << "time:" << dt;
+
+
+  time_t y = time(0);
+  tm *z = localtime(&y);
+  cout << "years ::" << z->tm_year+1900 << endl;
+  cout << "month ::" << z->tm_mon+1 << endl;
+
+  cout << "days week ::" << z->tm_wday << endl;
+  cout << "days month ::" << z->tm_mday << endl;
+
+  cout << "days year ::" << z->tm_yday << endl;
+  cout << "munite ::" << z->tm_min << endl;
+  cout << "hours ::" << z->tm_hour<< endl;
+
+
+}
